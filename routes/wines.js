cd9482742349b35536cc071bad5a46f8a65d5667
idoll <doll.ian@gmail.com>
2013-12-14 19:06:12 -0500

    work motherfucker

--- routes/wines.js
+++ routes/wines.js
@@ -4,7 +4,7 @@ var Server = mongo.Server,
     Db = mongo.Db,
     BSON = mongo.BSONPure;
 
-var server = new Server('localhost', 27017, {auto_reconnect: true});
+var server = new Server('mongodb://heroku_app20371317:q5mmrnihjhpnl6shc6u5s38vis@ds061228.mongolab.com:61228/heroku_app20371317', 27017, {auto_reconnect: true});
 db = new Db('winedb', server, {safe: true});
 
 db.open(function(err, db) {
